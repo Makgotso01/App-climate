8d90f04888a2edb7cb3f8fbef496c51802fb8ce5
Makgotso01 <111054182+Makgotso01@users.noreply.github.com>
2024-02-17 09:45:55 -0800

    added new files

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-
-  getForecast(response.data.city);
 }
+
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -39,11 +38,13 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+
 function searchCity(city) {
-  let apiKey = " dd0a3a53b7tb8edf05dbd4ce6o42b46b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
@@ -51,196 +52,7 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[date.getDay()];
-}
-
-function getForecast(city) {
-  let apiKey = "dd0a3a53b7tb8edf05dbd4ce6o42b46b";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios(apiUrl).then(displayForecast);
-}
-
-function displayForecast(response) {
-  let forecastHtml = "";
-
-  response.data.daily.forEach(function (day, index) {
-    if (index < 7) {
-      forecastHtml =
-        forecastHtml +
-        `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${formatDay(day.time)}</div>
-
-        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}º</strong>
-          </div>
-          <div class="weather-forecast-temperature">${Math.round(
-            day.temperature.minimum
-          )}º</div>
-        </div>
-      </div>
-    `;
-    }
-  });
-
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
-
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-
-const dayEl = document.querySelector(".default_day");
-const dateEl = document.querySelector(".default_date");
-const btnEl = document.querySelector(".btn_search");
-const inputEl = document.querySelector(".input_field");
-
-const iconsContainer = document.querySelector(".icons");
-const dayInfoEl = document.querySelector(".day_info");
-const listContentEl = document.querySelector(".list_content ul");
-
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-// display the day
-const day = new Date();
-const dayName = days[day.getDay()];
-dayEl.textContent = dayName;
-
-// display date
-let month = day.toLocaleString("default", { month: "long" });
-let date = day.getDate();
-let year = day.getFullYear();
-
-console.log();
-dateEl.textContent = date + " " + month + " " + year;
-
-// add event
-btnEl.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  // check empty value
-  if (inputEl.value !== "") {
-    const Search = inputEl.value;
-    inputEl.value = "";
-    findLocation(Search);
-  } else {
-    console.log("Please Enter City or Country Name");
-  }
-});
-
-async function findLocation(name) {
-  iconsContainer.innerHTML = "";
-  dayInfoEl.innerHTML = "";
-  listContentEl.innerHTML = "";
-  try {
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API}`;
-    const data = await fetch(API_URL);
-    const result = await data.json();
-    console.log(result);
-
-    if (result.cod !== "404") {
-      // display image content
-      const ImageContent = displayImageContent(result);
-
-      // display right side content
-      const rightSide = rightSideContent(result);
-
-      // forecast function
-      displayForeCast(result.coord.lat, result.coord.lon);
-
-      setTimeout(() => {
-        iconsContainer.insertAdjacentHTML("afterbegin", ImageContent);
-        iconsContainer.classList.add("fadeIn");
-        dayInfoEl.insertAdjacentHTML("afterbegin", rightSide);
-      }, 1500);
-    } else {
-      const message = `<h2 class="weather_temp">${result.cod}</h2>
-      <h3 class="cloudtxt">${result.message}</h3>`;
-      iconsContainer.insertAdjacentHTML("afterbegin", message);
-    }
-  } catch (error) {}
-}
-
-// display image content and temp
-function displayImageContent(data) {
-  return `<img src="https://openweathermap.org/img/wn/${
-    data.weather[0].icon
-  }@4x.png" alt="" />
-    <h2 class="weather_temp">${Math.round(data.main.temp - 275.15)}°C</h2>
-    <h3 class="cloudtxt">${data.weather[0].description}</h3>`;
-}
-
-// display the right side content
-function rightSideContent(result) {
-  return `<div class="content">
-          <p class="title">NAME</p>
-          <span class="value">${result.name}</span>
-        </div>
-        <div class="content">
-          <p class="title">TEMP</p>
-          <span class="value">${Math.round(result.main.temp - 275.15)}°C</span>
-        </div>
-        <div class="content">
-          <p class="title">HUMIDITY</p>
-          <span class="value">${result.main.humidity}%</span>
-        </div>
-        <div class="content">
-          <p class="title">WIND SPEED</p>
-          <span class="value">${result.wind.speed} Km/h</span>
-        </div>`;
-}
-
-async function displayForeCast(lat, long) {
-  const ForeCast_API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${API}`;
-  const data = await fetch(ForeCast_API);
-  const result = await data.json();
-  // filter the forecast
-  const uniqeForeCastDays = [];
-  const daysForecast = result.list.filter((forecast) => {
-    const forecastDate = new Date(forecast.dt_txt).getDate();
-    if (!uniqeForeCastDays.includes(forecastDate)) {
-      return uniqeForeCastDays.push(forecastDate);
-    }
-  });
-  console.log(daysForecast);
-
-  daysForecast.forEach((content, indx) => {
-    if (indx <= 3) {
-      listContentEl.insertAdjacentHTML("afterbegin", forecast(content));
-    }
-  });
-}
-
-// forecast html element data
-function forecast(frContent) {
-  const day = new Date(frContent.dt_txt);
-  const dayName = days[day.getDay()];
-  const splitDay = dayName.split("", 3);
-  const joinDay = splitDay.join("");
-
-  // console.log(dayName);
-
-  return `<li>
-  <img src="https://openweathermap.org/img/wn/${
-    frContent.weather[0].icon
-  }@2x.png" />
-  <span>${joinDay}</span>
-  <span class="day_temp">${Math.round(frContent.main.temp - 275.15)}°C</span>
-</li>`;
-}
